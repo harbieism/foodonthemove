25be928f46d749e1a300052028e97cb729125b12
harbieism <jow598@utulsa.edu>
2015-02-22 10:45:12 -0600

    Added a view that returns json of accounts

--- foodonthemove/views.py
+++ foodonthemove/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import renderm
+from django.shortcuts import render
 from foodonthemove.models import Account
-# Create your views here.
+from foodonthemove.serializers import AccountSerializer
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+
 def index(request):
     account_list = Account.objects.all()
+    context = {'account_list': account_list}
+    return render(request, 'foodonthemove/index.html', context)
+
+
+def list_ticket(request):
+    account_list = Account.objects.all()
+    serialized = AccountSerializer(account_list, many=True)
+    json_account = JSONRenderer().render(serialized.data)
+    print "Yes"
+    print json_account
+    return HttpResponse(json_account, 'application/json')
