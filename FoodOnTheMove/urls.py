e5ab3e417e94c0e6723ae4da35c5f164d7e5bf84
harbieism <jow598@utulsa.edu>
2015-02-22 10:40:34 -0600

    Add URLs for checkin page

--- FoodOnTheMove/urls.py
+++ FoodOnTheMove/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'FoodOnTheMove.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^food/', include('foodonthemove.urls'))
+    url(r'^food/', include('foodonthemove.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- foodonthemove/urls.py
+++ foodonthemove/urls.py
@@ -4,4 +4,5 @@ from foodonthemove import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^list_ticket/$', views.list_ticket, name='list')
 )
