9e3fa82f5eafdc6036994c7299f2a9b0ac062dc1
harbieism <jow598@utulsa.edu>
2015-02-25 13:12:28 -0600

    Add view to handle registration

--- foodonthemove/views.py
+++ foodonthemove/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from foodonthemove.models import Account
+from foodonthemove.forms import AccountForm
+from django.core.urlresolvers import reverse
 from foodonthemove.serializers import AccountSerializer
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from rest_framework.renderers import JSONRenderer
 
 def index(request):
@@ -9,6 +11,11 @@ def index(request):
     context = {'account_list': account_list}
     return render(request, 'foodonthemove/index.html', context)
 
+def get_account(username):
+    try:
+        return Account.objects.get(username=username)
+    except Account.DoesNotExist:
+        return None
 
 def list_ticket(request):
     account_list = Account.objects.all()
@@ -17,3 +24,61 @@ def list_ticket(request):
     print "Yes"
     print json_account
     return HttpResponse(json_account, 'application/json')
+
+def register(request):
+    if request.method == "POST":
+
+        email = request.POST.get("emailValue")
+        username = request.POST.get("username")
+        phone_number = request.POST.get("phoneNumber")
+        userType = request.POST.get("userType")
+        if userType == 0:
+            username = email
+        elif userType == 1:
+            username = phone_number
+
+        if (request.POST.get("contactEmail") == "on"):
+            contact_email = True
+        else:
+            contact_email = False
+
+        if (request.POST.get("contactCall") == "on"):
+            contact_call = True
+        else:
+            contact_call = False
+
+        if (request.POST.get("contactText") == "on"):
+            contact_text = True
+        else:
+            contact_text = False
+
+        first_name = request.POST.get("inputFirstname")
+        last_name = request.POST.get("inputLastname")
+
+        zip_code = request.POST.get("zipCode")
+
+        is_paying = request.POST.get('inputPayment')
+        payment_amount = request.POST.get('inputAmount')
+
+        meals = request.POST.get('inputMeals')
+        password = request.POST.get('passwordValue')
+        password_confirm = request.POST.get('passwordConfirmValue')
+        account = get_account(username)
+        if password == password_confirm:
+            if account is None:
+                account = Account.objects.create_user(
+                    username=username, email=email, phone_number=phone_number,
+                    contact_email=contact_email, contact_call=contact_call,
+                    contact_text=contact_text, first_name=first_name,
+                    last_name=last_name, zip_code=zip_code,
+                    is_paying=is_paying, payment_amount=payment_amount,
+                    password=password
+                )
+                print account
+
+        return HttpResponseRedirect(reverse('index'))
+
+
+    elif request.method == "GET":
+        form = AccountForm()
+        return render(request, 'foodonthemove/register.html', {'form': form})
