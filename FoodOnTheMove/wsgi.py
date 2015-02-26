9fe09c3cc1cb14015c0c546460b3fe62a6e126df
harbieism <jow598@utulsa.edu>
2015-02-25 18:12:02 -0600

    static hosted using cling

--- FoodOnTheMove/wsgi.py
+++ FoodOnTheMove/wsgi.py
@@ -12,7 +12,7 @@ os.environ["DJANGO_SETTINGS_MODULE"] =  "FoodOnTheMove.settings"
 
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
