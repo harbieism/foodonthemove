7f065749838c1066d63b049530ff5095bf9617a3
harbieism <jow598@utulsa.edu>
2015-02-26 11:33:29 -0600

    Added try except redirect back to login

--- foodonthemove/views.py
+++ foodonthemove/views.py
@@ -57,69 +57,71 @@ def list_ticket(request):
 
 def register(request):
     if request.method == "POST":
+        try:
+            email = request.POST.get("emailValue")
+            username = request.POST.get("username")
+            phone_number = request.POST.get("phoneNumber")
+            userType = request.POST.get("userType")
+            if int(userType) == 0:
+                username = str(email)
+            elif int(userType) == 1:
+                username = str(phone_number)
 
-        email = request.POST.get("emailValue")
-        username = request.POST.get("username")
-        phone_number = request.POST.get("phoneNumber")
-        userType = request.POST.get("userType")
-        if int(userType) == 0:
-            username = str(email)
-        elif int(userType) == 1:
-            username = str(phone_number)
+            if (request.POST.get("contactEmail") == "on"):
+                contact_email = True
+            else:
+                contact_email = False
 
-        if (request.POST.get("contactEmail") == "on"):
-            contact_email = True
-        else:
-            contact_email = False
+            if (request.POST.get("contactCall") == "on"):
+                contact_call = True
+            else:
+                contact_call = False
 
-        if (request.POST.get("contactCall") == "on"):
-            contact_call = True
-        else:
-            contact_call = False
+            if (request.POST.get("contactText") == "on"):
+                contact_text = True
+            else:
+                contact_text = False
 
-        if (request.POST.get("contactText") == "on"):
-            contact_text = True
-        else:
-            contact_text = False
+            first_name = request.POST.get("inputFirstname")
+            last_name = request.POST.get("inputLastname")
 
-        first_name = request.POST.get("inputFirstname")
-        last_name = request.POST.get("inputLastname")
+            zip_code = request.POST.get("zipCode")
 
-        zip_code = request.POST.get("zipCode")
+            is_paying = request.POST.get('inputPayment')
+            if is_paying == '0':
+                is_paying = False
+            else:
+                is_paying = True
 
-        is_paying = request.POST.get('inputPayment')
-        if is_paying == '0':
-            is_paying = False
-        else:
-            is_paying = True
+            payment_amount = request.POST.get('inputAmount')
+            meals = request.POST.get('inputMeals')
+            password = request.POST.get('passwordValue')
+            password_confirm = request.POST.get('passwordConfirmValue')
+            account = get_account(username)
+            if password == password_confirm:
+                if account is None:
+                    if is_paying:
+                        account = Account.objects.create_user(
+                            username=username, email=email, phone_number=phone_number,
+                            contact_email=contact_email, contact_call=contact_call,
+                            contact_text=contact_text, first_name=first_name,
+                            last_name=last_name, zip_code=zip_code,
+                            is_paying=is_paying, payment_amount=payment_amount,
+                            password=password
+                        )
+                    else:
+                        account = Account.objects.create_user(
+                            username=username, email=email, phone_number=phone_number,
+                            contact_email=contact_email, contact_call=contact_call,
+                            contact_text=contact_text, first_name=first_name,
+                            last_name=last_name, zip_code=zip_code,
+                            is_paying=is_paying,
+                            password=password
+                        )
 
-        payment_amount = request.POST.get('inputAmount')
-        meals = request.POST.get('inputMeals')
-        password = request.POST.get('passwordValue')
-        password_confirm = request.POST.get('passwordConfirmValue')
-        account = get_account(username)
-        if password == password_confirm:
-            if account is None:
-                if is_paying:
-                    account = Account.objects.create_user(
-                        username=username, email=email, phone_number=phone_number,
-                        contact_email=contact_email, contact_call=contact_call,
-                        contact_text=contact_text, first_name=first_name,
-                        last_name=last_name, zip_code=zip_code,
-                        is_paying=is_paying, payment_amount=payment_amount,
-                        password=password
-                    )
-                else:
-                    account = Account.objects.create_user(
-                        username=username, email=email, phone_number=phone_number,
-                        contact_email=contact_email, contact_call=contact_call,
-                        contact_text=contact_text, first_name=first_name,
-                        last_name=last_name, zip_code=zip_code,
-                        is_paying=is_paying,
-                        password=password
-                    )
-
-        return HttpResponseRedirect(reverse('index'))
+            return HttpResponseRedirect(reverse('register'))
+        except:
+            return HttpResponseRedirect(reverse('register'))
 
 
     elif request.method == "GET":
